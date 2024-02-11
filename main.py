08846153e5b020349314040fad9cf88dbbb4403d
Reid Farmer <reidfarmer@Reids-MacBook-Pro-3.local>
2024-02-10 21:49:20 -0700

    Added the checking of airline names and added them to the flights.json

--- main.py
+++ main.py
@@ -1,34 +1,45 @@
-# imports packages
-from urllib.request import (urlopen)
+from urllib.request import urlopen
 import json
+import time
 
-# empty list
-airplane = []
-
-# location of adsb json data from my local antenna
 url = "http://adsb.sirver.co:2222/tar1090/data/aircraft.json"
 
-first_response = urlopen(url)
+airplane = {}
+loop_count = 0
+primary_key = 'Hex'
 
-second_response = first_response.read()
+while loop_count < 20:
+    first_response = urlopen(url)
+    second_response = first_response.read()
+    json_data = json.loads(second_response)
 
-json_data = json.loads(second_response)
+    for data in json_data['aircraft']:
+        if {'flight', 'hex', 'alt_baro', 'lat', 'lon', 'gs'} <= data.keys():
+            # print(data['flight'], data['hex'])
+            spaced_flight_number = str(data['flight'])
+            flight_number = spaced_flight_number.replace(' ', '')
 
-for data in json_data['aircraft']:
-    if {'flight', 'hex', 'alt_baro', 'lat', 'lon'} <= data.keys():
-        # print(data['flight'], data['hex'])
-        spaced_flight_number = str(data['flight'])
-        flight_number = spaced_flight_number.replace(' ', '')
-        hex = data['hex']
-        altitude = data['alt_baro']
-        latitude = data['lat']
-        longitude = data['lon']
-        dict = {'Hex': hex, 'Flight': flight_number, 'Altitude': altitude, 'Latitude': latitude, 'Longitude': longitude}
-        airplane.append(dict)
-        # print(hex)
-        # airplane.append(flight_number)
+            hex = data['hex']
+            altitude = data['alt_baro']
+            latitude = data['lat']
+            longitude = data['lon']
+            ground_speed = data['gs']
 
-print(airplane)
+            airplane[hex] = {'Flight': flight_number, 'Altitude': altitude, 'Latitude': latitude, 'Longitude': longitude, 'Ground Speed': ground_speed}
+
+            # for witch in airplane:
+            #     if witch[primary_key] == dict[primary_key]:
+            #         print(f'Value exists: {dict}')
+            #     else:
+            #         airplane.append(dict)
+    loop_count += 1
+    time.sleep(2)
+            # print(hex)
+            # airplane.append(flight_number)
+
+# print(airplane)
+
+# print(airplane[list(airplane.keys())[1]])
 
 # create json object from the stored adsb list
 json_object = json.dumps(airplane, indent=1)
@@ -36,3 +47,37 @@ json_object = json.dumps(airplane, indent=1)
 # create the json file from the object
 with open("flights.json", "w") as outfile:
    outfile.write(json_object)
+
+Delta = 0
+American = 0
+Southwest = 0
+Other = 0
+Frontier = 0
+airlines = []
+
+for hex in airplane.keys():
+    # print(airplane[hex])
+
+    airline = airplane[hex]['Flight']
+    if 'SWA' in airline:
+        Southwest += 1
+    elif 'AAL' in airline:
+        American += 1
+    elif 'DAL' in airline:
+        Delta += 1
+    elif 'FFT' in airline:
+        Frontier += 1
+    else:
+        Other += 1
+
+dict = {'Southwest': Southwest, 'Delta': Delta, 'Frontier': Frontier, 'American': American, 'Other': Other}
+airlines.append(dict)
+
+print(airlines)
+# print(f'Southwest:{Southwest} \nAmerican:{American} \nDelta:{American} \nOther:{Other} \nFrontier:{Frontier}')
+
+another_json_object = json.dumps(airlines, indent=1)
+
+# create the json file from the object
+with open("airlines.json", "w") as outfile:
+   outfile.write(another_json_object)
--- test.py
+++ test.py
@@ -2,29 +2,46 @@ from urllib.request import urlopen
 import json
 import time
 
+with open('airline_list.json', 'r') as lots_of_airlines:
+    airline_list = json.load(lots_of_airlines)
+
+organized_airline = {}
+
+for airline in airline_list:
+    if airline['country'] == 'United States' or airline['country'] == 'Mexico':
+        if airline['icao'] != '':
+            organized_airline[airline['icao']] = airline['name']
+print(organized_airline)
+
 url = "http://adsb.sirver.co:2222/tar1090/data/aircraft.json"
 
 airplane = {}
 loop_count = 0
 primary_key = 'Hex'
 
-while loop_count < 20:
+while loop_count < 5:
     first_response = urlopen(url)
     second_response = first_response.read()
     json_data = json.loads(second_response)
 
     for data in json_data['aircraft']:
-        if {'flight', 'hex', 'alt_baro', 'lat', 'lon'} <= data.keys():
+        if {'flight', 'hex', 'alt_baro', 'lat', 'lon', 'gs'} <= data.keys():
             # print(data['flight'], data['hex'])
             spaced_flight_number = str(data['flight'])
             flight_number = spaced_flight_number.replace(' ', '')
+
+            airline_name = 'Unknown'
+            potential_icao = flight_number[0:3]
+            if potential_icao in organized_airline:
+                airline_name = organized_airline[potential_icao]
 
             hex = data['hex']
             altitude = data['alt_baro']
             latitude = data['lat']
             longitude = data['lon']
+            ground_speed = data['gs']
 
-            airplane[hex] = {'Flight': flight_number, 'Altitude': altitude, 'Latitude': latitude, 'Longitude': longitude}
+            airplane[hex] = {'Name': airline_name, 'Flight': flight_number, 'Altitude': altitude, 'Latitude': latitude, 'Longitude': longitude, 'Ground Speed': ground_speed}
 
             # for witch in airplane:
             #     if witch[primary_key] == dict[primary_key]:
@@ -32,7 +49,7 @@ while loop_count < 20:
             #     else:
             #         airplane.append(dict)
     loop_count += 1
-    time.sleep(2)
+    time.sleep(1)
             # print(hex)
             # airplane.append(flight_number)
 
@@ -78,5 +95,5 @@ print(airlines)
 another_json_object = json.dumps(airlines, indent=1)
 
 # create the json file from the object
-with open("airlines.json", "w") as outfile:
+with open('airlines.json', 'w') as outfile:
    outfile.write(another_json_object)
